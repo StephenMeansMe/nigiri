a9c2f4b9dd9072da7a170cdc7e3ac8ad78bef0fb
Shisai <strauss@muttisblumen.de>
2008-04-17 15:39:16 +0200

    fixed own_*-signals added connection retries if client is started before maki signals now have access to client-information

--- nigiri.py
+++ nigiri.py
@@ -7,6 +7,7 @@ import dbus
 import gobject
 import threading
 import sys
+import time
 import signals
 import commands
 
@@ -20,23 +21,12 @@ class Nigiri(threading.Thread):
     self.current_server = "euirc" # TODO
     self.current_channel = "#Inkirei" # TODO
     self.commandlist = commands.get_commandlist()
-  
-    print "connecting to dbus..."
-    DBusGMainLoop(set_as_default=True)
-    gobject.io_add_watch(sys.stdin, gobject.IO_IN | gobject.IO_ERR | gobject.IO_HUP, commands.input_handler, self)
-    self.loop = gobject.MainLoop()
-  
-    self.bus = dbus.SessionBus()
-    self.proxy = self.bus.get_object("de.ikkoku.sushi", "/de/ikkoku/sushi")
+    self.connection_retries = 12 # TODO client-config
+    self.bus = None
+    self.proxy = None
+    self.loop = None
+    self.start_client()
 
-    signals.add_all(self.bus)
-
-    print "starting signal processing..."
-
-    commands.servers(self, "")
-    commands.own_nick(self, "")
-    for server in self.servers:
-      commands.channels(self, server)
   def run(self):
     try:
       self.loop.run()
@@ -46,6 +36,36 @@ class Nigiri(threading.Thread):
   def stop(self):
     self._Thread__stop()
     self.loop.quit()
+
+  def start_client(self):
+    print "connecting to dbus..."
+    DBusGMainLoop(set_as_default=True)
+    gobject.io_add_watch(sys.stdin, gobject.IO_IN | gobject.IO_ERR | gobject.IO_HUP, commands.input_handler, self)
+    self.loop = gobject.MainLoop()
+    self.bus = dbus.SessionBus()
+  
+    print "starting signal processing..."
+    self.signals = signals.Signals(self)
+
+    retry = 0
+    while self.proxy == None and retry < self.connection_retries: 
+      print "connecting to maki..."
+      try:
+        self.proxy = self.bus.get_object("de.ikkoku.sushi", "/de/ikkoku/sushi")
+      except dbus.DBusException:
+        retry += 1
+        if retry < self.connection_retries:
+          time.sleep(5)
+          print "retry",
+        else:
+          print "connection failed!"
+          sys.exit(1)
+    
+    print "retrieve information from maki"
+    commands.servers(self, "")
+    commands.own_nick(self, "")
+    for server in self.servers:
+      commands.channels(self, server)
 
 if __name__ == "__main__":
   nigiri = Nigiri()
--- signals.py
+++ signals.py
@@ -8,119 +8,120 @@ import time
 def timestamp(line, timestamp):
   return time.strftime("[%H%M%S] ", time.localtime(timestamp)) + line
 
-def add_all(bus):
-  print "adding signals..."
-  bus.add_signal_receiver(message, "message")
-  bus.add_signal_receiver(own_message, "own_message")
-  bus.add_signal_receiver(query, "query")
-  bus.add_signal_receiver(own_query, "own_query")
-  bus.add_signal_receiver(notice, "notice")
-  bus.add_signal_receiver(action, "action")
-  bus.add_signal_receiver(ctcp, "ctcp")
-  bus.add_signal_receiver(away, "away")
-  bus.add_signal_receiver(away_message, "away_message")
-  bus.add_signal_receiver(back, "back")
-  bus.add_signal_receiver(join, "join")
-  bus.add_signal_receiver(part, "part")
-  bus.add_signal_receiver(kick, "kick")
-  bus.add_signal_receiver(nick, "nick")
-  bus.add_signal_receiver(mode, "mode")
-  bus.add_signal_receiver(connect, "connect")
-  bus.add_signal_receiver(reconnect, "reconnect")
-  bus.add_signal_receiver(connected, "connected")
-  bus.add_signal_receiver(topic, "topic")
-  bus.add_signal_receiver(motd, "motd")
-  bus.add_signal_receiver(quit, "quit")
-  bus.add_signal_receiver(shutdown, "shutdown")
-
 ###
 # Signal Handlers
 ###
-def message(time, server, nick, target, message):
-  print timestamp("<%s> %s" % (nick, message), time)
-
-def own_message(time, server, target, message):
-  print timestamp("<%s> %s" % ("FIXME", message), time)
-
-def query(time, server, nick, message):
-  print timestamp("<%s> %s" % (nick, message), time)
-
-def own_query(time, server, target, message):
-  print timestamp("<%s> %s" % ("FIXME", message), time)
-
-def notice(time, server, nick, target, message):
-  print timestamp("<<%s>> %s" % (nick, message), time)
-
-def action(time, server, nick, target, message):
-  print timestamp("*%s %s*" % (nick, message), time)
-
-def ctcp(time, server, nick, target, message):
-  print timestamp("??? ctcp-request for %s by %s" % (message, nick), time)
-
-def away(time, server):
-  print timestamp("You have been marked as being away", time)
-
-def away_message(time, server, nick, message):
-  print timestamp("%s is away (reason: %s)" % (nick, message), time)
-
-def back(time, server):
-  print timestamp("You are no longer marked as being away", time)
-
-def join(time, server, nick, channel):
-  "prints information about joins in the respective channels"
-  print timestamp(">>> %s joined %s on %s" % (nick, channel, server), time)
-
-def part(time, server, nick, channel, message):
-  "message ist optional und kann leer (\"\") sein."
-  print timestamp("<<< %s has left %s (reason: %s)" % (nick, channel, message), time)
-
-def kick(time, server, nick, channel, who, message):
-  "message ist optional und kann leer (\"\") sein."
-  print timestamp("-!- %s has been kicked by %s (reason: %s)" % (who, nick, message), time)
-
-def nick(time, server, nick, new_nick):
-  print timestamp("-!- %s is now known as %s" % (nick, new_nick), time)
-
-def mode(time, server, nick, target, mode, parameter):
-  """nick ist optional und kann leer(\"\") sein.
-parameter ist optional und kann leer(\"\") sein."""
-  if parameter != "":
-    space = " "
-  else:
-    space = ""
-
-  if nick == "":
-    print timestamp("-!- current mode of %s [%s%s%s]" % (target, mode, space, parameter), time)
-  else:
-    print timestamp("-!- %s changed mode of %s [%s%s%s]" % (nick, target, mode, space, parameter), time)
-
-def connect(time, server):
-  "Wird gesendet, wenn eine erfolgreiche Verbindung zum IRC-Server aufgebaut wurde."
-  print timestamp("/// client is trying to connect to %s" % server, time)
-
-def reconnect(time, server):
-  "Wird gesendet, wenn ein Wiederverbindungsversuch stattfindet."
-  print timestamp("\\\\\\ client is trying to reconnect to %s" % server, time)
-
-def connected(time, server, nick):
-  "Wird gesendet, wenn ein Login auf dem IRC-Server stattgefunden hat. nick ist dabei der eigene Nickname."
-  print timestamp("/// client is now connected to %s" % server, time)
-  print timestamp("    nick is set to %s" % nick, time)
-
-def topic(time, server, nick, channel, topic):
-  "nick ist optional und kann leer (\"\") sein."
-  if nick == "":
-    print timestamp("||| the current topic is: %s" % topic, time)
-  else:
-    print timestamp("||| %s changed the topic to: %s" (nick, topic), time)
-
-def motd(time, server, message):
-  print message
-
-def quit(time, server, nick, message):
-  "message ist optional und kann leer (\"\") sein."
-  print timestamp("<<< %s has quit (reason: %s)" % (nick, message), time)
-
-def shutdown(time):
-  "Wird gesendet, wenn maki beendet wird."
-  print timestamp("!!! client is being shut down", time)
+class Signals:
+  def __init__(self, nigiri):
+    self.nigiri = nigiri
+    self.nigiri.bus.add_signal_receiver(self.message, "message")
+    self.nigiri.bus.add_signal_receiver(self.own_message, "own_message")
+    self.nigiri.bus.add_signal_receiver(self.query, "query")
+    self.nigiri.bus.add_signal_receiver(self.own_query, "own_query")
+    self.nigiri.bus.add_signal_receiver(self.notice, "notice")
+    self.nigiri.bus.add_signal_receiver(self.action, "action")
+    self.nigiri.bus.add_signal_receiver(self.ctcp, "ctcp")
+    self.nigiri.bus.add_signal_receiver(self.away, "away")
+    self.nigiri.bus.add_signal_receiver(self.away_message, "away_message")
+    self.nigiri.bus.add_signal_receiver(self.back, "back")
+    self.nigiri.bus.add_signal_receiver(self.join, "join")
+    self.nigiri.bus.add_signal_receiver(self.part, "part")
+    self.nigiri.bus.add_signal_receiver(self.kick, "kick")
+    self.nigiri.bus.add_signal_receiver(self.nick, "nick")
+    self.nigiri.bus.add_signal_receiver(self.mode, "mode")
+    self.nigiri.bus.add_signal_receiver(self.connect, "connect")
+    self.nigiri.bus.add_signal_receiver(self.reconnect, "reconnect")
+    self.nigiri.bus.add_signal_receiver(self.connected, "connected")
+    self.nigiri.bus.add_signal_receiver(self.topic, "topic")
+    self.nigiri.bus.add_signal_receiver(self.motd, "motd")
+    self.nigiri.bus.add_signal_receiver(self.quit, "quit")
+    self.nigiri.bus.add_signal_receiver(self.shutdown, "shutdown")
+  
+  def message(self, time, server, nick, target, message):
+    print timestamp("<%s> %s" % (nick, message), time)
+  
+  def own_message(self, time, server, target, message):
+    print timestamp("<%s> %s" % (self.nigiri.own_nicks[server], message), time)
+  
+  def query(self, time, server, nick, message):
+    print timestamp("<%s> %s" % (nick, message), time)
+  
+  def own_query(self, time, server, target, message):
+    print timestamp("<%s> %s" % (self.nigiri.own_nicks[server], message), time)
+  
+  def notice(self, time, server, nick, target, message):
+    print timestamp("<<%s>> %s" % (nick, message), time)
+  
+  def action(self, time, server, nick, target, message):
+    print timestamp("*%s %s*" % (nick, message), time)
+  
+  def ctcp(self, time, server, nick, target, message):
+    print timestamp("??? ctcp-request for %s by %s" % (message, nick), time)
+  
+  def away(self, time, server):
+    print timestamp("You have been marked as being away", time)
+  
+  def away_message(self, time, server, nick, message):
+    print timestamp("%s is away (reason: %s)" % (nick, message), time)
+  
+  def back(self, time, server):
+    print timestamp("You are no longer marked as being away", time)
+  
+  def join(self, time, server, nick, channel):
+    "prints information about joins in the respective channels"
+    print timestamp(">>> %s joined %s on %s" % (nick, channel, server), time)
+  
+  def part(self, time, server, nick, channel, message):
+    "message ist optional und kann leer (\"\") sein."
+    print timestamp("<<< %s has left %s (reason: %s)" % (nick, channel, message), time)
+  
+  def kick(self, time, server, nick, channel, who, message):
+    "message ist optional und kann leer (\"\") sein."
+    print timestamp("-!- %s has been kicked by %s (reason: %s)" % (who, nick, message), time)
+  
+  def nick(self, time, server, nick, new_nick):
+    print timestamp("-!- %s is now known as %s" % (nick, new_nick), time)
+  
+  def mode(self, time, server, nick, target, mode, parameter):
+    """nick ist optional und kann leer(\"\") sein.
+       parameter ist optional und kann leer(\"\") sein."""
+    if parameter != "":
+      space = " "
+    else:
+      space = ""
+  
+    if nick == "":
+      print timestamp("-!- current mode of %s [%s%s%s]" % (target, mode, space, parameter), time)
+    else:
+      print timestamp("-!- %s changed mode of %s [%s%s%s]" % (nick, target, mode, space, parameter), time)
+  
+  def connect(self, time, server):
+    "Wird gesendet, wenn eine erfolgreiche Verbindung zum IRC-Server aufgebaut wurde."
+    print timestamp("/// client is trying to connect to %s" % server, time)
+  
+  def reconnect(self, time, server):
+    "Wird gesendet, wenn ein Wiederverbindungsversuch stattfindet."
+    print timestamp("\\\\\\ client is trying to reconnect to %s" % server, time)
+  
+  def connected(self, time, server, nick):
+    "Wird gesendet, wenn ein Login auf dem IRC-Server stattgefunden hat. nick ist dabei der eigene Nickname."
+    print timestamp("/// client is now connected to %s" % server, time)
+    print timestamp("    nick is set to %s" % nick, time)
+  
+  def topic(self, time, server, nick, channel, topic):
+    "nick ist optional und kann leer (\"\") sein."
+    if nick == "":
+      print timestamp("||| the current topic is: %s" % topic, time)
+    else:
+      print timestamp("||| %s changed the topic to: %s" (nick, topic), time)
+  
+  def motd(self, time, server, message):
+    print message
+  
+  def quit(self, time, server, nick, message):
+    "message ist optional und kann leer (\"\") sein."
+    print timestamp("<<< %s has quit (reason: %s)" % (nick, message), time)
+  
+  def shutdown(self, time):
+    "Wird gesendet, wenn maki beendet wird."
+    print timestamp("!!! client is being shut down", time)
